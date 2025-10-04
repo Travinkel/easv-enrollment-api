07048150082b75ad7d6908f8d84f4cd8e80c2389
Travinkel <ankersoe@protonmail.com>
2025-10-04 14:35:20 +0200

    test(api): add edge-case tests for double confirm, double cancel, confirm-after-cancel

--- tests/Enrollment.IntegrationTests/EnrollmentEdgeCaseTests.cs
+++ tests/Enrollment.IntegrationTests/EnrollmentEdgeCaseTests.cs
@@ -23,6 +23,54 @@ public class EnrollmentEdgeCaseTests : IntegrationTestBase
         res2.StatusCode.Should().Be(HttpStatusCode.Conflict);
     }
 
+    [Fact]
+    public async Task Confirm_Twice_Returns_409_Conflict()
+    {
+        var req = new EnrollmentRequest(Guid.NewGuid(), Guid.NewGuid());
+        var created = await Client.PostAsJsonAsync("/enrollments", req);
+        var enrollment = await created.Content.ReadFromJsonAsync<EnrollmentResponse>(Json);
+        
+        // First confirm + OK
+        var firstConfirm = await Client.PostAsync($"/enrollments/{enrollment!.Id}/confirm", null);
+        firstConfirm.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        // Second confirm + Conflict
+        var secondConfirm = await Client.PostAsync($"/enrollments/{enrollment.Id}/confirm", null);
+        secondConfirm.StatusCode.Should().Be(HttpStatusCode.Conflict); 
+    }
+
+    [Fact]
+    public async Task Cancel_Twice_Returns_409_Conflict()
+    {
+        var req = new EnrollmentRequest(Guid.NewGuid(), Guid.NewGuid());
+        var created = await Client.PostAsJsonAsync("/enrollments", req);
+        var enrollment = await created.Content.ReadFromJsonAsync<EnrollmentResponse>(Json);
+        
+        // First cancel + OK
+        var firstCancel = await Client.PostAsync($"/enrollments/{enrollment!.Id}/cancel", null);
+        firstCancel.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        // Second cancel + Conflict
+        var secondCancel = await Client.PostAsync($"/enrollments/{enrollment.Id}/cancel", null);
+        secondCancel.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
+    [Fact]
+    public async Task Confirm_After_Cancel_Returns_409_Conflict()
+    {
+        var req = new EnrollmentRequest(Guid.NewGuid(), Guid.NewGuid());
+        var created = await Client.PostAsJsonAsync("/enrollments", req);
+        var enrollment = await created.Content.ReadFromJsonAsync<EnrollmentResponse>(Json);
+        
+        // First cancel + OK
+        var cancel = await Client.PostAsync($"/enrollments/{enrollment!.Id}/cancel", null);
+        cancel.StatusCode.Should().Be(HttpStatusCode.OK);
+        
+        // Second confirm + Conflict
+        var confirm = await Client.PostAsync($"/enrollments/{enrollment.Id}/confirm", null);
+        confirm.StatusCode.Should().Be(HttpStatusCode.Conflict);
+    }
+
     [Theory]
     [InlineData("not-a-guid", "baddata-123")] // both invalid
     [InlineData("00000000-0000-0000-0000-000000000000", "00000000-0000-0000-0000-000000000000")] // empty guids
